1fbfc162865ceab2ed6bde51295b05d4ad5861ef
Seatom <seatom@qq.com>
2021-07-11 16:27:11 +0800

    up

--- jd_fruit.js
+++ jd_fruit.js
@@ -1238,7 +1238,7 @@ function timeFormat(time) {
 function readShareCode() {
   console.log(`开始`)
   return new Promise(async resolve => {
-    $.get({url: "https://cdn.jsdelivr.net/gh/seatom/updateTeam@main/shareCodes/JD_Fruit.json",headers:{
+    $.get({url: "https://ghproxy.com/https://raw.githubusercontent.com/seatom/updateTeam/main/shareCodes/JD_Fruit.json",headers:{
         "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
       }}, async (err, resp, data) => {
       try {
