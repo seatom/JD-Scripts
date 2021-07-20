ddb00da0490c87597666be82256c81180b6d561d
Seatom <seatom@qq.com>
2021-07-20 22:52:06 +0800

    up

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -52,6 +52,8 @@
 30 6 * * * node /scripts/jd_goodMorning.js >> /scripts/logs/jd_goodMorning.log 2>&1
 #发财大赢家
 30 9,15,21 * * * node /scripts/jd_fcdyj.js >> /scripts/logs/jd_fcdyj.log 2>&1
+# 跳跳乐瓜分京豆
+1 3,11,21 * * * node /scripts/jd_jump.js >> /scripts/logs/jd_jump.log 2>&1
 ##############长期活动##############
 # 签到
 0 0,18 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
